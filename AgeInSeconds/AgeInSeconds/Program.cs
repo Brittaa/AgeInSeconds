b18383f7438aebadb918d11962b1a354797eed97
Britta <Britta@Britta-MacBook-Air.local>
2019-11-04 21:00:51 +0200

    AgeApp

---
+++ AgeInSeconds/AgeInSeconds/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace AgeInSeconds
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hello, please enter your birthdate(YYYY/MM/DD): ");
+            string birthday = Console.ReadLine();
+            Console.WriteLine($"You are born {birthday}.");
+
+            GetAge(birthday);
+
+        }
+        public static void GetAge (string birthday)
+        {
+            string[] personalData = birthday.Split('/');
+            int year = int.Parse(personalData[0]);
+            int month = int.Parse(personalData[1]);
+            int day = int.Parse(personalData[2]);
+
+            
+            DateTime userBirthday = new DateTime(year, month, day);
+            
+
+            TimeSpan ages = DateTime.Now - userBirthday;
+            var seconds = Math.Round(ages.TotalSeconds);
+
+            Console.WriteLine($"You are {seconds} seconds old.");
+        }
+    }
+}
